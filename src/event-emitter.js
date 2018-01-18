c8911ba024a7cc59c2a49f176a04a8b3d499ecfd
LvChengbin <lvchengbin@nextseason.cc>
2018-01-18 22:39:30 +0800

    upgrade

--- src/event-emitter.js
+++ src/event-emitter.js
@@ -50,7 +50,7 @@ export default class EventEmitter {
         const handlers = this.__listeners[ evt ];
         if( handlers ) {
             for( let i = 0, l = handlers.length; i < l; i += 1 ) {
-                handlers[ i ] && handlers[ i ]( ...args );
+                handlers[ i ] && handlers[ i ].call( this, ...args );
             }
             return true;
         }
