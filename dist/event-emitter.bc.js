f23a4c85ebb28717c0c711e3310b83c5e556971e
LvChengbin <lvchengbin@nextseason.cc>
2018-02-13 18:35:35 +0800

    changed babel to preset-env

--- dist/event-emitter.bc.js
+++ dist/event-emitter.bc.js
@@ -4,6 +4,28 @@
 	(global.EventEmitter = factory());
 }(this, (function () { 'use strict';
 
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
 var isString = (function (str) {
   return typeof str === 'string' || str instanceof String;
 });
@@ -20,131 +42,114 @@ var isRegExp = (function (reg) {
   return {}.toString.call(reg) === '[object RegExp]';
 });
 
-var classCallCheck = function (instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-};
-
-var createClass = function () {
-  function defineProperties(target, props) {
-    for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];
-      descriptor.enumerable = descriptor.enumerable || false;
-      descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
-      Object.defineProperty(target, descriptor.key, descriptor);
-    }
+var EventEmitter =
+/*#__PURE__*/
+function () {
+  function EventEmitter() {
+    _classCallCheck(this, EventEmitter);
+    this.__listeners = {};
   }
 
-  return function (Constructor, protoProps, staticProps) {
-    if (protoProps) defineProperties(Constructor.prototype, protoProps);
-    if (staticProps) defineProperties(Constructor, staticProps);
-    return Constructor;
-  };
-}();
-
-var EventEmitter = function () {
-    function EventEmitter() {
-        classCallCheck(this, EventEmitter);
-
-        this.__listeners = {};
+  _createClass(EventEmitter, [{
+    key: "alias",
+    value: function alias(name, to) {
+      this[name] = this[to].bind(this);
     }
+  }, {
+    key: "on",
+    value: function on(evt, handler) {
+      var listeners = this.__listeners;
+      listeners[evt] ? listeners[evt].push(handler) : listeners[evt] = [handler];
+      return this;
+    }
+  }, {
+    key: "once",
+    value: function once(evt, handler) {
+      var _this = this;
 
-    createClass(EventEmitter, [{
-        key: 'alias',
-        value: function alias(name, to) {
-            this[name] = this[to].bind(this);
+      var _handler = function _handler() {
+        for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+          args[_key] = arguments[_key];
         }
-    }, {
-        key: 'on',
-        value: function on(evt, handler) {
-            var listeners = this.__listeners;
-            listeners[evt] ? listeners[evt].push(handler) : listeners[evt] = [handler];
-            return this;
+
+        handler.apply(_this, args);
+
+        _this.removeListener(evt, _handler);
+      };
+
+      return this.on(evt, _handler);
+    }
+  }, {
+    key: "removeListener",
+    value: function removeListener(evt, handler) {
+      var listeners = this.__listeners,
+          handlers = listeners[evt];
+
+      if (!handlers || !handlers.length) {
+        return this;
+      }
+
+      for (var i = 0; i < handlers.length; i += 1) {
+        handlers[i] === handler && (handlers[i] = null);
+      }
+
+      setTimeout(function () {
+        for (var _i = 0; _i < handlers.length; _i += 1) {
+          handlers[_i] || handlers.splice(_i--, 1);
         }
-    }, {
-        key: 'once',
-        value: function once(evt, handler) {
-            var _this = this;
+      }, 0);
+      return this;
+    }
+  }, {
+    key: "emit",
+    value: function emit(evt) {
+      var handlers = this.__listeners[evt];
 
-            var _handler = function _handler() {
-                for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-                    args[_key] = arguments[_key];
-                }
-
-                handler.apply(_this, args);
-                _this.removeListener(evt, _handler);
-            };
-            return this.on(evt, _handler);
+      if (handlers) {
+        for (var _len2 = arguments.length, args = new Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+          args[_key2 - 1] = arguments[_key2];
         }
-    }, {
-        key: 'removeListener',
-        value: function removeListener(evt, handler) {
-            var listeners = this.__listeners,
-                handlers = listeners[evt];
 
-            if (!handlers || !handlers.length) {
-                return this;
-            }
+        for (var i = 0, l = handlers.length; i < l; i += 1) {
+          var _handlers$i;
 
-            for (var i = 0; i < handlers.length; i += 1) {
-                handlers[i] === handler && (handlers[i] = null);
-            }
-
-            setTimeout(function () {
-                for (var _i = 0; _i < handlers.length; _i += 1) {
-                    handlers[_i] || handlers.splice(_i--, 1);
-                }
-            }, 0);
-
-            return this;
+          handlers[i] && (_handlers$i = handlers[i]).call.apply(_handlers$i, [this].concat(args));
         }
-    }, {
-        key: 'emit',
-        value: function emit(evt) {
-            var handlers = this.__listeners[evt];
-            if (handlers) {
-                for (var _len2 = arguments.length, args = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
-                    args[_key2 - 1] = arguments[_key2];
-                }
 
-                for (var i = 0, l = handlers.length; i < l; i += 1) {
-                    var _handlers$i;
+        return true;
+      }
 
-                    handlers[i] && (_handlers$i = handlers[i]).call.apply(_handlers$i, [this].concat(args));
-                }
-                return true;
-            }
-            return false;
+      return false;
+    }
+  }, {
+    key: "removeAllListeners",
+    value: function removeAllListeners(rule) {
+      var checker;
+
+      if (isString(rule)) {
+        checker = function checker(name) {
+          return rule === name;
+        };
+      } else if (isFunction(rule)) {
+        checker = rule;
+      } else if (isRegExp(rule)) {
+        checker = function checker(name) {
+          rule.lastIndex = 0;
+          return rule.test(name);
+        };
+      }
+
+      var listeners = this.__listeners;
+
+      for (var attr in listeners) {
+        if (checker(attr)) {
+          listeners[attr] = null;
+          delete listeners[attr];
         }
-    }, {
-        key: 'removeAllListeners',
-        value: function removeAllListeners(rule) {
-            var checker = void 0;
-            if (isString(rule)) {
-                checker = function checker(name) {
-                    return rule === name;
-                };
-            } else if (isFunction(rule)) {
-                checker = rule;
-            } else if (isRegExp(rule)) {
-                checker = function checker(name) {
-                    rule.lastIndex = 0;
-                    return rule.test(name);
-                };
-            }
-
-            var listeners = this.__listeners;
-            for (var attr in listeners) {
-                if (checker(attr)) {
-                    listeners[attr] = null;
-                    delete listeners[attr];
-                }
-            }
-        }
-    }]);
-    return EventEmitter;
+      }
+    }
+  }]);
+  return EventEmitter;
 }();
 
 return EventEmitter;
