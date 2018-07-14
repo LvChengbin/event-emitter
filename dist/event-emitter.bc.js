f02481cc7cd56f24fbb7e3360c3ec4706d4de874
LvChengbin <lvchengbin@nextseason.cc>
2018-07-14 22:19:29 +0800

    upgrade

--- dist/event-emitter.bc.js
+++ dist/event-emitter.bc.js
@@ -4,197 +4,6 @@
 	(global.EventEmitter = factory());
 }(this, (function () { 'use strict';
 
-function isUndefined() {
-    return arguments.length > 0 && typeof arguments[ 0 ] === 'undefined';
-}
-
-function find( haystack, key ) {
-    for( var i = 0, list = haystack; i < list.length; i += 1 ) {
-        var item = list[i];
-
-        if( item[ 0 ] === key ) { return item; }
-    }
-    return false;
-}
-
-var Map = function Map( iterable ) {
-    if ( iterable === void 0 ) iterable = [];
-
-    if( !( this instanceof Map ) ) {
-        throw new TypeError( 'Constructor Map requires \'new\'' );
-    }
-    this.map = iterable || [];
-};
-
-var prototypeAccessors = { size: { configurable: true } };
-prototypeAccessors.size.get = function () {
-    return this.map.length;
-};
-
-Map.prototype.get = function get ( key ) {
-    var data = find( this.map, key );
-    return data ? data[ 1 ] : undefined;
-};
-
-Map.prototype.set = function set ( key, value ) {
-    var data = find( this.map, key );
-    if( data ) {
-        data[ 1 ] = value;
-    } else {
-        this.map.push( [ key, value ] );
-    }
-    return this;
-};
-
-Map.prototype.delete = function delete$1 ( key ) {
-        var this$1 = this;
-
-    for( var i = 0, l = this.map.length; i < l; i += 1 ) {
-        var item = this$1.map[ i ];
-        if( item[ 0 ] === key ) {
-            this$1.map.splice( i, 1 );
-            return true;
-        }
-            
-    }
-    return false;
-};
-
-Map.prototype.clear = function clear () {
-    this.map= [];
-};
-
-Map.prototype.forEach = function forEach ( callback, thisArg ) {
-        var this$1 = this;
-
-    isUndefined( thisArg ) && ( this.Arg = this );
-    for( var i = 0, list = this$1.map; i < list.length; i += 1 ) {
-        var item = list[i];
-
-            callback.call( thisArg, item[ 1 ], item[ 0 ], this$1 );
-    }
-};
-
-Map.prototype.has = function has ( key ) {
-    return !!find( this.map, key );
-};
-
-Map.prototype.keys = function keys () {
-        var this$1 = this;
-
-    var keys = [];
-    for( var i = 0, list = this$1.map; i < list.length; i += 1 ) {
-        var item = list[i];
-
-            keys.push( item[ 0 ] );
-    }
-    return keys;
-};
-
-Map.prototype.entries = function entries () {
-    return this.map;
-};
-
-Map.prototype.values = function values () {
-        var this$1 = this;
-
-    var values = [];
-    for( var i = 0, list = this$1.map; i < list.length; i += 1 ) {
-        var item = list[i];
-
-            values.push( item[ 1 ] );
-    }
-    return values;
-};
-
-Object.defineProperties( Map.prototype, prototypeAccessors );
-
-var Set = function Set( iterable ) {
-    var this$1 = this;
-    if ( iterable === void 0 ) iterable = [];
-
-    if( !( this instanceof Set ) ) {
-        throw new TypeError( 'Constructor Set requires \'new\'' );
-    }
-    this.set = [];
-
-    if( iterable && iterable.length ) {
-        for( var i = 0, list = iterable; i < list.length; i += 1 ) {
-            var item = list[i];
-
-            this$1.add( item );
-        }
-    }
-};
-
-var prototypeAccessors$1 = { size: { configurable: true } };
-
-prototypeAccessors$1.size.get = function () {
-    return this.set.length;
-};
-
-Set.prototype.add = function add ( value ) {
-    var i = this.set.indexOf( value );
-    if( i > -1 ) {
-        this.set[ i ] = value;
-    } else {
-        this.set.push( value );
-    }
-    return this;
-};
-
-Set.prototype.delete = function delete$1 ( value ) {
-    var i = this.set.indexOf( value );
-    if( i > -1 ) {
-        this.set.splice( i, 1 );
-        return true;
-    }
-    return false;
-};
-
-Set.prototype.clear = function clear () {
-    this.set = [];
-};
-
-Set.prototype.forEach = function forEach ( callback, thisArg ) {
-        var this$1 = this;
-
-    isUndefined( thisArg ) && ( this.Arg = this );
-    for( var i = 0, list = this$1.set; i < list.length; i += 1 ) {
-        var item = list[i];
-
-            callback.call( thisArg, item, item, this$1 );
-    }
-};
-
-Set.prototype.has = function has ( value ) {
-    return this.set.indexOf( value ) > -1;
-};
-
-Set.prototype.keys = function keys () {
-    return this.values();
-};
-
-Set.prototype.entries = function entries () {
-        var this$1 = this;
-
-    var res = [];
-    for( var i = 0, list = this$1.set; i < list.length; i += 1 ) {
-        var item = list[i];
-
-            res.push( [ item, item ] ); 
-    }
-    return res;
-};
-
-Set.prototype.values = function values () {
-    return this.set;
-};
-
-Object.defineProperties( Set.prototype, prototypeAccessors$1 );
-
-function isString (str) { return typeof str === 'string' || str instanceof String; }
-
 /**
  * async function
  *
@@ -214,10 +23,331 @@ function isAsyncFunction (fn) { return ( {} ).toString.call( fn ) === '[object A
 
 function isFunction (fn) { return ({}).toString.call( fn ) === '[object Function]' || isAsyncFunction( fn ); }
 
+function isUndefined() {
+    return arguments.length > 0 && typeof arguments[ 0 ] === 'undefined';
+}
+
+var defineProperty = Object.defineProperty;
+var methods = [ 'clear', 'delete', 'entries', 'forEach', 'get', 'has', 'keys', 'set', 'values' ];
+
+var supportNativeMap = function () {
+    if( typeof Map === 'undefined' ) { return false; }
+    for( var i = 0, list = methods; i < list.length; i += 1 ) {
+        var method = list[i];
+
+        if( !isFunction( Map.prototype[ method ] ) ) { return false; }
+    }
+    return true;
+};
+
+function find( haystack, key ) {
+    for( var i = 0, list = haystack; i < list.length; i += 1 ) {
+        var item = list[i];
+
+        if( item[ 0 ] === key ) { return item; }
+    }
+    return false;
+}
+
+var M = function M( iterable, nativeMap ) {
+    if ( iterable === void 0 ) iterable = [];
+    if ( nativeMap === void 0 ) nativeMap = true;
+
+    if( !( this instanceof M ) ) {
+        throw new TypeError( 'Constructor Map requires \'new\'' );
+    }
+
+    if( nativeMap && supportNativeMap() ) {
+        return new Map( iterable );
+    }
+
+    var map = iterable || [];
+
+    defineProperty( map, 'size', {
+        enumerable : false,
+        get: function get() {
+            return this.length;
+        }
+    } );
+
+    defineProperty( map, 'get', {
+        enumerable : false,
+        value : function( key ) {
+            var data = find( this, key );
+            return data ? data[ 1 ] : undefined;
+        }
+    } );
+
+    defineProperty( map, 'set', {
+        enumerable : false,
+        value : function( key, value ) {
+            var data = find( this, key );
+            if( data ) {
+                data[ 1 ] = value;
+            } else {
+                this.push( [ key, value ] );
+            }
+            return this;
+        }
+    } );
+
+    defineProperty( map, 'delete', {
+        enumerable : false,
+        value : function( key ) {
+            var this$1 = this;
+
+            for( var i = 0, l = this.length; i < l; i += 1 ) {
+                if( this$1[ i ][ 0 ] === key ) {
+                    this$1.splice( i, 1 );
+                    return true;
+                }
+            }
+            return false;
+        }
+    } );
+
+    defineProperty( map, 'clear', {
+        enumerable : false,
+        value : function() {
+            this.length = 0;
+        }
+    } );
+
+    defineProperty( map, 'forEach', {
+        enumerable : false,
+        value : function( callback, thisArg ) {
+            var this$1 = this;
+
+            isUndefined( thisArg ) && ( thisArg = this );
+            for( var i = 0, list = this$1; i < list.length; i += 1 ) {
+                var item = list[i];
+
+                callback.call( thisArg, item[ 1 ], item[ 0 ], this$1 );
+            }
+        }
+    } );
+
+    defineProperty( map, 'has', {
+        enumerable : false,
+        value : function( key ) {
+            return !!find( this, key );
+        }
+    } );
+
+    defineProperty( map, 'keys', {
+        enumerable : false,
+        value : function() {
+            var this$1 = this;
+
+            var keys = [];
+            for( var i = 0, list = this$1; i < list.length; i += 1 ) {
+                var item = list[i];
+
+                keys.push( item[ 0 ] );
+            }
+            return keys;
+        }
+    } );
+
+    defineProperty( map, 'entries', {
+        enumerable : false,
+        value : function() {
+            return this;
+        }
+    } );
+
+    defineProperty( map, 'values', {
+        enumerable : false,
+        value : function() {
+            var this$1 = this;
+
+            var values = [];
+            for( var i = 0, list = this$1; i < list.length; i += 1 ) {
+                var item = list[i];
+
+                values.push( item[ 1 ] );
+            }
+            return values;
+        }
+    } );
+    return map;
+};
+
+/**
+ * async function
+ *
+ * @syntax: 
+ *  async function() {}
+ *  async () => {}
+ *  async x() => {}
+ *
+ * @compatibility
+ * IE: no
+ * Edge: >= 15
+ * Android: >= 5.0
+ *
+ */
+
+function isAsyncFunction$1 (fn) { return ( {} ).toString.call( fn ) === '[object AsyncFunction]'; }
+
+function isFunction$1 (fn) { return ({}).toString.call( fn ) === '[object Function]' || isAsyncFunction$1( fn ); }
+
+function isUndefined$1() {
+    return arguments.length > 0 && typeof arguments[ 0 ] === 'undefined';
+}
+
+var defineProperty$1 = Object.defineProperty;
+var g = typeof global === 'undefined' ? window : global;
+var methods$1 = [ 'add', 'clear', 'delete', 'entries', 'forEach', 'has', 'values' ];
+
+var supportNativeSet = function () {
+    if( !g.Set ) { return false; }
+    for( var i = 0, list = methods$1; i < list.length; i += 1 ) {
+        var method = list[i];
+
+        if( !isFunction$1( Set.prototype[ method ] ) ) { return false; }
+    }
+    return true;
+};
+
+var S = function S( iterable, nativeSet ) {
+    if ( iterable === void 0 ) iterable = [];
+    if ( nativeSet === void 0 ) nativeSet = true;
+
+    if( nativeSet && supportNativeSet() ) {
+        return new g.Set( iterable );
+    }
+
+    if( !( this instanceof S ) ) {
+        throw new TypeError( 'Constructor Set requires \'new\'' );
+    }
+
+    var set = [];
+
+    defineProperty$1( set, 'size', {
+        enumerable : false,
+        get: function get() {
+            return set.length;
+        }
+    } );
+
+    defineProperty$1( set, 'add', {
+        enumerable : false,
+        value : function( value ) {
+            var i = this.indexOf( value );
+            if( i > -1 ) {
+                this[ i ] = value;
+            } else {
+                this.push( value );
+            }
+            return this;
+        }
+    } );
+
+    defineProperty$1( set, 'delete', {
+        enumerable : false,
+        value : function( value ) {
+            var i = this.indexOf( value );
+            if( i > -1 ) {
+                this.splice( i, 1 );
+                return true;
+            }
+            return false;
+        }
+    } );
+
+    defineProperty$1( set, 'clear', {
+        enumerable : false,
+        value : function() {
+            this.length = 0;
+        }
+    } );
+
+    defineProperty$1( set, 'forEach', {
+        enumerable : false,
+        value : function( callback, thisArg ) {
+            var this$1 = this;
+
+            isUndefined$1( thisArg ) && ( thisArg = this );
+            for( var i = 0, list = this$1; i < list.length; i += 1 ) {
+                var item = list[i];
+
+                callback.call( thisArg, item, item, this$1 );
+            }
+        }
+    } );
+
+    defineProperty$1( set, 'has', {
+        enumerable : false,
+        value : function( value ) {
+            return this.indexOf( value ) > -1;
+        }
+    } );
+
+    defineProperty$1( set, 'keys', {
+        enumerable : false,
+        value : function() {
+            return this.values();
+        }
+    } );
+
+    defineProperty$1( set, 'entries', {
+        enumerable : false,
+        value : function() {
+            var this$1 = this;
+
+            var res = [];
+            for( var i = 0, list = this$1; i < list.length; i += 1 ) {
+                var item = list[i];
+
+                res.push( [ item, item ] ); 
+            }
+            return res;
+        }
+    } );
+
+    defineProperty$1( set, 'values', {
+        enumerable : false,
+        value : function() {
+            return this;
+        }
+    } );
+
+    if( iterable ) {
+        for( var i = 0, list = iterable; i < list.length; i += 1 ) {
+            var item = list[i];
+
+            set.add( item );
+        }
+    }
+    return set;
+};
+
+function isString (str) { return typeof str === 'string' || str instanceof String; }
+
+/**
+ * async function
+ *
+ * @syntax: 
+ *  async function() {}
+ *  async () => {}
+ *  async x() => {}
+ *
+ * @compatibility
+ * IE: no
+ * Edge: >= 15
+ * Android: >= 5.0
+ *
+ */
+
+function isAsyncFunction$2 (fn) { return ( {} ).toString.call( fn ) === '[object AsyncFunction]'; }
+
+function isFunction$2 (fn) { return ({}).toString.call( fn ) === '[object Function]' || isAsyncFunction$2( fn ); }
+
 function isRegExp (reg) { return ({}).toString.call( reg ) === '[object RegExp]'; }
 
 var defaultExport = function defaultExport() {
-    this.__listeners = new Map();
+    this.__listeners = new M();
 };
 
 defaultExport.prototype.on = function on ( evt, handler ) {
@@ -225,7 +355,7 @@ defaultExport.prototype.on = function on ( evt, handler ) {
     var handlers = listeners.get( evt );
 
     if( !handlers ) {
-        handlers = new Set();
+        handlers = new S();
         listeners.set( evt, handlers );
     }
     handlers.add( handler );
@@ -266,7 +396,7 @@ defaultExport.prototype.removeAllListeners = function removeAllListeners ( rule 
     var checker;
     if( isString( rule ) ) {
         checker = function (name) { return rule === name; };
-    } else if( isFunction( rule ) ) {
+    } else if( isFunction$2( rule ) ) {
         checker = rule;
     } else if( isRegExp( rule ) ) {
         checker = function (name) {
--- dist/event-emitter.js
+++ dist/event-emitter.js
@@ -4,9 +4,39 @@
 	(global.EventEmitter = factory());
 }(this, (function () { 'use strict';
 
+/**
+ * async function
+ *
+ * @syntax: 
+ *  async function() {}
+ *  async () => {}
+ *  async x() => {}
+ *
+ * @compatibility
+ * IE: no
+ * Edge: >= 15
+ * Android: >= 5.0
+ *
+ */
+
+var isAsyncFunction = fn => ( {} ).toString.call( fn ) === '[object AsyncFunction]';
+
+var isFunction = fn => ({}).toString.call( fn ) === '[object Function]' || isAsyncFunction( fn );
+
 function isUndefined() {
     return arguments.length > 0 && typeof arguments[ 0 ] === 'undefined';
 }
+
+const defineProperty = Object.defineProperty;
+const methods = [ 'clear', 'delete', 'entries', 'forEach', 'get', 'has', 'keys', 'set', 'values' ];
+
+const supportNativeMap = () => {
+    if( typeof Map === 'undefined' ) return false;
+    for( const method of methods ) {
+        if( !isFunction( Map.prototype[ method ] ) ) return false;
+    }
+    return true;
+};
 
 function find( haystack, key ) {
     for( let item of haystack ) {
@@ -15,144 +45,247 @@ function find( haystack, key ) {
     return false;
 }
 
-class Map {
-    constructor( iterable = [] ) {
-        if( !( this instanceof Map ) ) {
+class M {
+    constructor( iterable = [], nativeMap = true ) {
+        if( !( this instanceof M ) ) {
             throw new TypeError( 'Constructor Map requires \'new\'' );
         }
-        this.map = iterable || [];
-    }
-    get size() {
-        return this.map.length;
-    }
 
-    get( key ) {
-        const data = find( this.map, key );
-        return data ? data[ 1 ] : undefined;
-    }
-
-    set( key, value ) {
-        const data = find( this.map, key );
-        if( data ) {
-            data[ 1 ] = value;
-        } else {
-            this.map.push( [ key, value ] );
+        if( nativeMap && supportNativeMap() ) {
+            return new Map( iterable );
         }
-        return this;
-    }
 
-    delete( key ) {
-        for( let i = 0, l = this.map.length; i < l; i += 1 ) {
-            const item = this.map[ i ];
-            if( item[ 0 ] === key ) {
-                this.map.splice( i, 1 );
-                return true;
+        const map = iterable || [];
+
+        defineProperty( map, 'size', {
+            enumerable : false,
+            get() {
+                return this.length;
             }
-            
-        }
-        return false;
-    }
+        } );
 
-    clear() {
-        this.map= [];
-    }
+        defineProperty( map, 'get', {
+            enumerable : false,
+            value : function( key ) {
+                const data = find( this, key );
+                return data ? data[ 1 ] : undefined;
+            }
+        } );
 
-    forEach( callback, thisArg ) {
-        isUndefined( thisArg ) && ( this.Arg = this );
-        for( let item of this.map ) {
-            callback.call( thisArg, item[ 1 ], item[ 0 ], this );
-        }
-    }
+        defineProperty( map, 'set', {
+            enumerable : false,
+            value : function( key, value ) {
+                const data = find( this, key );
+                if( data ) {
+                    data[ 1 ] = value;
+                } else {
+                    this.push( [ key, value ] );
+                }
+                return this;
+            }
+        } );
 
-    has( key ) {
-        return !!find( this.map, key );
-    }
+        defineProperty( map, 'delete', {
+            enumerable : false,
+            value : function( key ) {
+                for( let i = 0, l = this.length; i < l; i += 1 ) {
+                    if( this[ i ][ 0 ] === key ) {
+                        this.splice( i, 1 );
+                        return true;
+                    }
+                }
+                return false;
+            }
+        } );
 
-    keys() {
-        const keys = [];
-        for( let item of this.map ) {
-            keys.push( item[ 0 ] );
-        }
-        return keys;
-    }
+        defineProperty( map, 'clear', {
+            enumerable : false,
+            value : function() {
+                this.length = 0;
+            }
+        } );
 
-    entries() {
-        return this.map;
-    }
+        defineProperty( map, 'forEach', {
+            enumerable : false,
+            value : function( callback, thisArg ) {
+                isUndefined( thisArg ) && ( thisArg = this );
+                for( let item of this ) {
+                    callback.call( thisArg, item[ 1 ], item[ 0 ], this );
+                }
+            }
+        } );
 
-    values() {
-        const values = [];
-        for( let item of this.map ) {
-            values.push( item[ 1 ] );
-        }
-        return values;
+        defineProperty( map, 'has', {
+            enumerable : false,
+            value : function( key ) {
+                return !!find( this, key );
+            }
+        } );
+
+        defineProperty( map, 'keys', {
+            enumerable : false,
+            value : function() {
+                const keys = [];
+                for( let item of this ) {
+                    keys.push( item[ 0 ] );
+                }
+                return keys;
+            }
+        } );
+
+        defineProperty( map, 'entries', {
+            enumerable : false,
+            value : function() {
+                return this;
+            }
+        } );
+
+        defineProperty( map, 'values', {
+            enumerable : false,
+            value : function() {
+                const values = [];
+                for( let item of this ) {
+                    values.push( item[ 1 ] );
+                }
+                return values;
+            }
+        } );
+        return map;
     }
 }
 
-class Set {
-    constructor( iterable = [] ) {
-        if( !( this instanceof Set ) ) {
+/**
+ * async function
+ *
+ * @syntax: 
+ *  async function() {}
+ *  async () => {}
+ *  async x() => {}
+ *
+ * @compatibility
+ * IE: no
+ * Edge: >= 15
+ * Android: >= 5.0
+ *
+ */
+
+var isAsyncFunction$1 = fn => ( {} ).toString.call( fn ) === '[object AsyncFunction]';
+
+var isFunction$1 = fn => ({}).toString.call( fn ) === '[object Function]' || isAsyncFunction$1( fn );
+
+function isUndefined$1() {
+    return arguments.length > 0 && typeof arguments[ 0 ] === 'undefined';
+}
+
+const defineProperty$1 = Object.defineProperty;
+const g = typeof global === 'undefined' ? window : global;
+const methods$1 = [ 'add', 'clear', 'delete', 'entries', 'forEach', 'has', 'values' ];
+
+const supportNativeSet = () => {
+    if( !g.Set ) return false;
+    for( let method of methods$1 ) {
+        if( !isFunction$1( Set.prototype[ method ] ) ) return false;
+    }
+    return true;
+};
+
+class S {
+    constructor( iterable = [], nativeSet = true ) {
+        if( nativeSet && supportNativeSet() ) {
+            return new g.Set( iterable );
+        }
+
+        if( !( this instanceof S ) ) {
             throw new TypeError( 'Constructor Set requires \'new\'' );
         }
-        this.set = [];
 
-        if( iterable && iterable.length ) {
-            for( let item of iterable ) this.add( item );
+        const set = [];
+
+        defineProperty$1( set, 'size', {
+            enumerable : false,
+            get() {
+                return set.length;
+            }
+        } );
+
+        defineProperty$1( set, 'add', {
+            enumerable : false,
+            value : function( value ) {
+                const i = this.indexOf( value );
+                if( i > -1 ) {
+                    this[ i ] = value;
+                } else {
+                    this.push( value );
+                }
+                return this;
+            }
+        } );
+
+        defineProperty$1( set, 'delete', {
+            enumerable : false,
+            value : function( value ) {
+                const i = this.indexOf( value );
+                if( i > -1 ) {
+                    this.splice( i, 1 );
+                    return true;
+                }
+                return false;
+            }
+        } );
+
+        defineProperty$1( set, 'clear', {
+            enumerable : false,
+            value : function() {
+                this.length = 0;
+            }
+        } );
+
+        defineProperty$1( set, 'forEach', {
+            enumerable : false,
+            value : function( callback, thisArg ) {
+                isUndefined$1( thisArg ) && ( thisArg = this );
+                for( let item of this ) {
+                    callback.call( thisArg, item, item, this );
+                }
+            }
+        } );
+
+        defineProperty$1( set, 'has', {
+            enumerable : false,
+            value : function( value ) {
+                return this.indexOf( value ) > -1;
+            }
+        } );
+
+        defineProperty$1( set, 'keys', {
+            enumerable : false,
+            value : function() {
+                return this.values();
+            }
+        } );
+
+        defineProperty$1( set, 'entries', {
+            enumerable : false,
+            value : function() {
+                const res = [];
+                for( let item of this ) {
+                    res.push( [ item, item ] ); 
+                }
+                return res;
+            }
+        } );
+
+        defineProperty$1( set, 'values', {
+            enumerable : false,
+            value : function() {
+                return this;
+            }
+        } );
+
+        if( iterable ) {
+            for( const item of iterable ) set.add( item );
         }
-    }
-
-    get size() {
-        return this.set.length;
-    }
-
-    add( value ) {
-        const i = this.set.indexOf( value );
-        if( i > -1 ) {
-            this.set[ i ] = value;
-        } else {
-            this.set.push( value );
-        }
-        return this;
-    }
-
-    delete( value ) {
-        const i = this.set.indexOf( value );
-        if( i > -1 ) {
-            this.set.splice( i, 1 );
-            return true;
-        }
-        return false;
-    }
-
-    clear() {
-        this.set = [];
-    }
-
-    forEach( callback, thisArg ) {
-        isUndefined( thisArg ) && ( this.Arg = this );
-        for( let item of this.set ) {
-            callback.call( thisArg, item, item, this );
-        }
-    }
-
-    has( value ) {
-        return this.set.indexOf( value ) > -1;
-    }
-
-    keys() {
-        return this.values();
-    }
-
-    entries() {
-        const res = [];
-        for( let item of this.set ) {
-            res.push( [ item, item ] ); 
-        }
-        return res;
-    }
-
-    values() {
-        return this.set;
+        return set;
     }
 }
 
@@ -173,15 +306,15 @@ var isString = str => typeof str === 'string' || str instanceof String;
  *
  */
 
-var isAsyncFunction = fn => ( {} ).toString.call( fn ) === '[object AsyncFunction]';
+var isAsyncFunction$2 = fn => ( {} ).toString.call( fn ) === '[object AsyncFunction]';
 
-var isFunction = fn => ({}).toString.call( fn ) === '[object Function]' || isAsyncFunction( fn );
+var isFunction$2 = fn => ({}).toString.call( fn ) === '[object Function]' || isAsyncFunction$2( fn );
 
 var isRegExp = reg => ({}).toString.call( reg ) === '[object RegExp]';
 
 class eventEmitter {
     constructor() {
-        this.__listeners = new Map();
+        this.__listeners = new M();
     }
 
     on( evt, handler ) {
@@ -189,7 +322,7 @@ class eventEmitter {
         let handlers = listeners.get( evt );
 
         if( !handlers ) {
-            handlers = new Set();
+            handlers = new S();
             listeners.set( evt, handlers );
         }
         handlers.add( handler );
@@ -221,7 +354,7 @@ class eventEmitter {
         let checker;
         if( isString( rule ) ) {
             checker = name => rule === name;
-        } else if( isFunction( rule ) ) {
+        } else if( isFunction$2( rule ) ) {
             checker = rule;
         } else if( isRegExp( rule ) ) {
             checker = name => {
